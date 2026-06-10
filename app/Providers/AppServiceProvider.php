27307187cd9840ab0c40db90fe26d5b67ab93d82
makinity <marklionesios@gmail.com>
2026-06-10 12:12:44 +0800

    Fix minor issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\Ipcr;
 use App\Observers\IpcrObserver;
+use App\Services\AssignmentAi\AssignmentPredictorInterface;
+use App\Services\AssignmentAi\SimulatedAssignmentPredictor;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,5 +13,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Ipcr::observe(IpcrObserver::class);
+    }
+
+    public function register(): void
+    {
+        $this->app->bind(AssignmentPredictorInterface::class, SimulatedAssignmentPredictor::class);
     }
 }
