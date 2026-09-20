6afeaeccd4507751cc3eb1c233c8b540cce0041c
makinity <marklionesios@gmail.com>
2026-09-20 21:20:59 +0800

    Implement Smart-PMS Icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>{{ config('app.name') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/pms-logo.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/pms-logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/pms-logo.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/pms-logo.png') }}">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('images/pms-logo.png') }}">
+        <link rel="apple-touch-icon" href="{{ asset('images/pms-logo.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
